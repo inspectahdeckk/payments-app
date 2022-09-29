6b6222c2e877ee987f78a7397dc6de09f8c90bdd
mateo <mateo.plava@gmail.com>
2022-09-29 18:28:43 +0200

    a

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use payments_engine::PaymentsEngine;
+use serde::Deserialize;
+use std::env;
+use std::error::Error;
+use std::ffi::OsString;
+
+#[derive(Debug, Deserialize)]
+struct Record {
+    r#type: String,
+    client: u16,
+    tx: u16,
+    amount: f32,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file_path = get_first_arg()?;
+    let mut rdr = csv::ReaderBuilder::new()
+        //.has_headers(false)
+        .trim(csv::Trim::All)
+        .from_path(file_path)?;
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
 }
